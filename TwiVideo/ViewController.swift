e8970ff18227ae35149313bfefc9ac67872ab051
AlbusSong <strawberry1234561@gmail.com>
2020-07-27 14:59:03 +0300

    Initial Commit

---
+++ TwiVideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwiVideo
+//
+//  Created by Albus on 7/27/20.
+//  Copyright © 2020 Albus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
